6c06a4877d4d4692e8bcefa711d581eb5b8f9df9
iqbalgn72 <iqbalgn72@gmail.com>
2024-05-14 14:51:47 +0700

    int mahasiswa dan nim

---
+++ statik.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa
+{
+public:
+    static int nim;
+    int id;
+    string nama;
+
+    void setID();
+
+    void printAll();
+    mahasiswa(string pnama)
+    {
+        nama = pnama;
+        setID();
+    }
+};
+
+int mahasiswa::nim = 221;
+
+void mahasiswa::setID()
+{
+    id = ++nim;
+}
+
+void mahasiswa::printAll()
+{
+    cout << "ID    = " << id << endl;
+    cout << "Nama  = " << nama << endl;
+    cout << endl;
+}
